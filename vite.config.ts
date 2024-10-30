1db46ec8353f25c7ab8a7b8bd5aca236bf955a86
Petr Romantsov <romantsovp@gmail.com>
2024-10-30 20:54:19 +0300

    bag fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
+  build: {
+    outDir: './build',
+  },
   plugins: [react()],
   test: {
     globals: true,
